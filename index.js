0bf11454aab74b5f600c4b368cfcabc022276ad0
Sandy <peacesandy04@gmail.com>
2023-03-05 13:46:18 -0800

    Added Question 2 to Stutern Assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+// Programming Languages
+
+let language1 = 'c#'
+let language2 = 'Javascript'
+let language3 = 'Ruby'
+let language4 = 'php'
+
+let languages = ["c#", "javascript", "Ruby", "php"]
+
+languages.push("Kotlin")
+languages.splice(3, 0, "Java")
+const removedElementFirst = languages.shift();
+languages.push("Go", "Rust")
+
+console.log(languages)
